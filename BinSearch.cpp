bfc6cd8cb31a277a5b25ca1f228fbf8955b765bb
WyattD21 <Wyatt127207@outlook.com>
2024-11-04 19:36:37 -0500

    Search

--- BinSearch.cpp
+++ BinSearch.cpp
@@ -2,8 +2,19 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+template<typename T>
+string toString(const T& value) {
+    ostringstream oss;
+    oss << value;
+    return oss.str();
+}
 
 // Recursive Binary Search
 int recursiveBinarySearch(const vector<int>& vec, int target, int left, int right) {
@@ -16,7 +27,7 @@ int recursiveBinarySearch(const vector<int>& vec, int target, int left, int righ
 
 // Iterative Binary Search
 int iterativeBinarySearch(const vector<int>& vec, int target) {
-    int left = 0, right = vec.size() - 1;
+    int left = 0, right = static_cast<int>(vec.size()) - 1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (vec[mid] == target) return mid;
@@ -28,38 +39,54 @@ int iterativeBinarySearch(const vector<int>& vec, int target) {
 
 // Sequential Search
 int sequentialSearch(const vector<int>& vec, int target) {
-    for (size_t i = 0; i < vec.size(); ++i) {
+    for (int i = 0; i < static_cast<int>(vec.size()); ++i) {
         if (vec[i] == target) return i;
     }
     return -1;
 }
 
 int main() {
-    vector<int> vec = { 3, 5, 8, 12, 15, 20, 25, 30 }; //data
-    sort(vec.begin(), vec.end());
-    int target1 = 15; // Test vals
-    int target2 = 100; // 
+    srand(static_cast<unsigned int>(time(0)));
 
-    // Recursive Binary Search
-    int index = recursiveBinarySearch(vec, target1, 0, vec.size() - 1);
-    cout << target1 << (index != -1 ? " found at location " : " was not found, return value is ") << index << endl;
+    vector<int> N_values = { 5000, 50000, 100000, 150000, 200000 }; // N
 
-    index = recursiveBinarySearch(vec, target2, 0, vec.size() - 1);
-    cout << target2 << (index != -1 ? " found at location " : " was not found, return value is ") << index << endl;
+    for (int N : N_values) {
+        double SumRBS = 0.0, SumIBS = 0.0, SumSeqS = 0.0;
 
-    // Iterative Binary Search
-    index = iterativeBinarySearch(vec, target1);
-    cout << target1 << (index != -1 ? " found at location " : " was not found, return value is ") << index << endl;
+        for (int i = 0; i < 10; ++i) {
+            vector<int> vec(N);
+            for (int& num : vec) {
+                num = rand() % 100 + 1;
+            }
+            sort(vec.begin(), vec.end());
+            int target = rand() % 100 + 1; 
 
-    index = iterativeBinarySearch(vec, target2);
-    cout << target2 << (index != -1 ? " found at location " : " was not found, return value is ") << index << endl;
+            // Timing Recursive Binary Search
+            auto start = chrono::high_resolution_clock::now();
+            recursiveBinarySearch(vec, target, 0, static_cast<int>(vec.size()) - 1);
+            auto end = chrono::high_resolution_clock::now();
+            SumRBS += chrono::duration_cast<chrono::microseconds>(end - start).count();
 
-    // Sequential Search
-    index = sequentialSearch(vec, target1);
-    cout << target1 << (index != -1 ? " found at location " : " was not found, return value is ") << index << endl;
+            // Timing Iterative Binary Search
+            start = chrono::high_resolution_clock::now();
+            iterativeBinarySearch(vec, target);
+            end = chrono::high_resolution_clock::now();
+            SumIBS += chrono::duration_cast<chrono::microseconds>(end - start).count();
 
-    index = sequentialSearch(vec, target2);
-    cout << target2 << (index != -1 ? " found at location " : " was not found, return value is ") << index << endl;
+            // Timing Sequential Search
+            start = chrono::high_resolution_clock::now();
+            sequentialSearch(vec, target);
+            end = chrono::high_resolution_clock::now();
+            SumSeqS += chrono::duration_cast<chrono::microseconds>(end - start).count();
+        }
+
+        // Display
+        cout << "For N = " << N << ":\n";
+        cout << "Average Running Time for Recursive Binary Search in microseconds is " << (SumRBS / 10) << endl;
+        cout << "Average Running Time for Iterative Binary Search in microseconds is " << (SumIBS / 10) << endl;
+        cout << "Average Running Time for Sequential Search in microseconds is " << (SumSeqS / 10) << endl;
+        cout << endl;
+    }
 
     return 0;
 }
